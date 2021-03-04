f9410ea6dacbbaf91985d168f40dc776c1cfd193
agustina ohan <agustina.oh@gmail.com>
2021-03-04 12:52:32 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'cocktails/show'
-  get 'cocktails/index'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources:cocktails, only: ["index", "new", "show", "create"]
+  resources :cocktails, only: %w[index new show create] do
+    resources :doses, only: %w[new create]
+  end
 end
