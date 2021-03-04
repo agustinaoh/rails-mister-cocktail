045c6fe2683c2fe037a734f86a355172e3abbbda
agustina ohan <agustina.oh@gmail.com>
2021-03-04 14:59:42 +0100

    controllers and views done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: %w[index new show create] do
     resources :doses, only: %w[new create]
   end
+
+  delete 'doses/:id', to: 'doses#destroy', as: :destroy
 end
